ab344632026ef271ef332dc21e0cbf8075d211d8
Shivam Amin <shivamamin2@gmail.com>
2023-05-09 21:16:39 -0400

    uh

--- weatherApp.js
+++ weatherApp.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const request = require('request');
 const express = require("express"); /* Accessing express module */
 const app = express(); /* app is a request handler function */
 const bodyParser = require("body-parser"); /* To handle post parameters */
@@ -6,11 +7,13 @@ require("dotenv").config({ path: path.resolve(__dirname, 'credentialsDontPost/.e
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { table } = require("console");
 const userName = process.env.MONGO_DB_USERNAME;
-const password = process.env.MONGO_DB_PASSWORD;;
+const password = process.env.MONGO_DB_PASSWORD;
 const databaseAndCollection = { db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION };
 const uri = `mongodb+srv://${userName}:${password}@cluster0.pnvigqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const $ = require('jquery');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 process.stdin.setEncoding("utf8");
 const portNumber = process.argv[2];/*takes in the portNum*/
@@ -34,15 +37,78 @@ process.stdin.on("readable", function () {/*stop function*/
     }
 });
 
-/*TEMPLATE WORK*/
 
+/*TEMPLATE WORK*/
 /* directory where templates will reside */
 app.set("views", path.resolve(__dirname, "templates"));
-
 /* view/templating engine */
 app.set("view engine", "ejs");
+
+
+
+
+/* ALL THE ROUTES BELOW */
 
 app.get("/", (request, response) => {
     /* Generating the HTML using index template */
     response.render("index");
 });
+
+app.get("/weather", (request, response) => {
+    response.render("weatherForm", {portNumber:portNumber})
+});
+
+app.get("/history", (request, response) => {
+    const variables = {
+        portNumber: portNumber
+    };
+    response.render("history", {portNumber:portNumber})
+});
+
+
+//  e/9k+BzpkolTusi8jdQU4g==cVSRjIhblPEMRkKb
+app.post("/weatherResults", async (request, response) => {  
+    const city = request.body.city;
+    let weatherData;
+
+    //get weather data
+
+
+    // add to database
+    const uri = `mongodb+srv://${userName}:${password}@cluster0.0fzphbg.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+    try {
+        await client.connect();
+        await insertWeather(client, databaseAndCollection, weatherData);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+    //render new page
+  
+    const variables = {
+        city:city,
+        cloud_pct: weatherData.cloud_pct,
+        temp: weatherData.temp,
+        feels_like: weatherData.feels_like,
+        humidity: weatherData.humidity,
+        min_temp: weatherData.min_temp,
+        max_temp: weatherData.max_temp,
+        wind_speed: weatherData.wind_speed,
+        wind_degrees: weatherData.wind_degrees,
+        sunrise: weatherData.sunrise,
+        sunset: weatherData.sunset
+    };
+      /* Generating the HTML using displayItems template */
+      response.render("displayWeather", variables);
+  });
+
+
+
+async function insertWeather(client, databaseAndCollection, weatherData) {
+    const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(weatherData);
+}
